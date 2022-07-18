8c9db95e6374185eee632107d763760c1c420b18
Dai Kindaichi <kindai.0911@gmail.com>
2022-07-18 14:26:35 +0900

    add b-tree index to post_id, created_at on comments table

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -233,6 +233,7 @@ module Isuconp
 
     get '/' do
       p "---------------"
+      p config
       me = get_session_user()
 
       results = db.query('SELECT `id`, `user_id`, `body`, `created_at`, `mime` FROM `posts` ORDER BY `created_at` DESC')
